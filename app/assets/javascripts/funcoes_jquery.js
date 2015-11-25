ea87b21b099abc852a0f508d88932783fe37351d
fellypeavelino <fellypeavelino@hotmail.com>
2015-11-25 02:04:26 -0200

    fourth

--- app/assets/javascripts/funcoes_jquery.js
+++ app/assets/javascripts/funcoes_jquery.js
@@ -1,4 +1,12 @@
 $(function(){
 	$('.actions input').attr('class','btn btn-success');
-
+    $(document).mousemove(function(event) {
+        TweenLite.to($("body"), 
+        .5, {
+            css: {
+                backgroundPosition: "" + parseInt(event.pageX / 8) + "px " + parseInt(event.pageY / '12') + "px, " + parseInt(event.pageX / '15') + "px " + parseInt(event.pageY / '15') + "px, " + parseInt(event.pageX / '30') + "px " + parseInt(event.pageY / '30') + "px",
+            	"background-position": parseInt(event.pageX / 8) + "px " + parseInt(event.pageY / 12) + "px, " + parseInt(event.pageX / 15) + "px " + parseInt(event.pageY / 15) + "px, " + parseInt(event.pageX / 30) + "px " + parseInt(event.pageY / 30) + "px"
+            }
+        })
+    })
 });
