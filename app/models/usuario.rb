d9b84ddd0a8f9a199a82baa570c22d48cc524af3
fellypeavelino <fellypeavelino@hotmail.com>
2015-11-26 14:52:03 -0200

    eighth

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -4,17 +4,19 @@ class Usuario < ActiveRecord::Base
   
 	validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create}, uniqueness: {case_sensitive: false}
 	
-	def nomeSexo(s)
-		if s == 'm' then
+	def nomeSexo
+		if read_attribute(:sexo) == 'm' then
 			return 'masculino'
 		end
-		if s == 'f' then
+		if read_attribute(:sexo) == 'f' then
 			return 'feminino'
 		end				
 	end
 	
-	def formataData(d)
-		return d.to_s.split('-').reverse.join('/')
+	def formataData
+		@nascimento = read_attribute(:nascimento)
+		write_attribute(:nascimento, @nascimento.to_s.split('-').reverse.join('/'))
+		#return d.to_s.split('-').reverse.join('/')
 	end
 	
 end
